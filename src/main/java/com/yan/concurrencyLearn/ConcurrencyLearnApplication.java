f44432491dc5f12ea0d809d776ef8b754d661a34
yanqingxin <2533818@qq.com>
2018-12-28 23:18:03 +0800

    first commit

---
+++ src/main/java/com/yan/concurrencyLearn/ConcurrencyLearnApplication.java
@@ -0,0 +1,14 @@
+package com.yan.concurrencyLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyLearnApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/yan/concurrencyLearn/ConcurrencyLearnApplicationTests.java
@@ -0,0 +1,17 @@
+package com.yan.concurrencyLearn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConcurrencyLearnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
